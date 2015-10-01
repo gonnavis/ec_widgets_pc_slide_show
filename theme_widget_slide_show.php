ddea9527db31ea2602b233d2c1796c105b7951d8
gonnavis <gonnavis@163.com>
2015-10-02 01:06:32 +0800

    v3

---
+++ theme_widget_slide_show.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @auther   ShopEx UED Jxwinter
+ * @date   2011-11-18
+ * @copyright  Copyright (c) 2005-2012 ShopEx Technologies Inc. (http://www.shopex.cn)
+ *
+ */
+
+
+function theme_widget_slide_show(&$setting,&$system){
+
+  return $setting;
+}
+?>
+
+
